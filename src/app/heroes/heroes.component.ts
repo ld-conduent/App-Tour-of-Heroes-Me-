c222b153d972ac809feaf7c260802e8916b8aa69
ld-conduent <121825896+ld-conduent@users.noreply.github.com>
2023-01-10 09:45:35 -0600

    Agregar navegacion Clean Completed

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,14 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+
 import { Hero } from '../hero';
 import { HeroService } from '../hero.service';
 
 @Component({
-  selector: 'app-dashboard',
-  templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.css'],
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css'],
 })
-export class DashboardComponent implements OnInit {
-  heroes: Hero[] = [];
+export class HeroesComponent implements OnInit {
+  heroes: Hero[];
 
   constructor(private heroService: HeroService) {}
 
@@ -17,8 +18,6 @@ export class DashboardComponent implements OnInit {
   }
 
   getHeroes(): void {
-    this.heroService
-      .getHeroes()
-      .subscribe((heroes) => (this.heroes = heroes.slice(1, 5)));
+    this.heroService.getHeroes().subscribe((heroes) => (this.heroes = heroes));
   }
 }
