d8cb050585cb6a1524d05c1a3af37428429e508f
ld-conduent <121825896+ld-conduent@users.noreply.github.com>
2023-01-10 12:48:42 -0600

    Navigation bar separate Completed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import { HeroSearchComponent } from './hero-search/hero-search.component';
 import { MessagesComponent } from './messages/messages.component';
+import { NavHeroComponent } from './nav-hero/nav-hero.component';
 
 @NgModule({
   imports: [
@@ -36,6 +37,7 @@ import { MessagesComponent } from './messages/messages.component';
     HeroDetailComponent,
     MessagesComponent,
     HeroSearchComponent,
+    NavHeroComponent,
   ],
   bootstrap: [AppComponent],
 })
---
+++ src/app/nav-hero/nav-hero.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-nav-hero',
+  templateUrl: './nav-hero.component.html',
+  styleUrls: ['./nav-hero.component.css']
+})
+export class NavHeroComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
