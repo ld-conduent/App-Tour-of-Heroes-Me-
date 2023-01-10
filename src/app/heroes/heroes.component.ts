ac7955c45cec3d024c25b8d7ebdf84e9de362d7d
ld-conduent <121825896+ld-conduent@users.noreply.github.com>
2023-01-10 09:34:34 -0600

    Agregar navegacion Completed

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,32 +1,24 @@
 import { Component, OnInit } from '@angular/core';
-import { HeroService } from '../hero.service';
 import { Hero } from '../hero';
-import { MessageService } from '../message.service';
+import { HeroService } from '../hero.service';
 
 @Component({
-  selector: 'app-heroes',
-  templateUrl: './heroes.component.html',
-  styleUrls: ['./heroes.component.css'],
+  selector: 'app-dashboard',
+  templateUrl: './dashboard.component.html',
+  styleUrls: ['./dashboard.component.css'],
 })
-export class HeroesComponent implements OnInit {
-  heroes: Hero[];
-  selectedHero: Hero;
+export class DashboardComponent implements OnInit {
+  heroes: Hero[] = [];
 
-  constructor(
-    private heroService: HeroService,
-    private messageService: MessageService
-  ) {}
+  constructor(private heroService: HeroService) {}
 
   ngOnInit() {
     this.getHeroes();
   }
 
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
-  }
-
   getHeroes(): void {
-    this.heroService.getHeroes().subscribe((heroes) => (this.heroes = heroes));
+    this.heroService
+      .getHeroes()
+      .subscribe((heroes) => (this.heroes = heroes.slice(1, 5)));
   }
 }
